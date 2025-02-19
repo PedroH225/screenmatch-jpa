ac106d886f0726add143d96c71f724796577098e
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-02-19 12:36:23 -0300

    Ordenar por categoria

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,10 +3,12 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.model.Episodio;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
@@ -40,12 +42,15 @@ public class Principal {
 
 			switch (opcao) {
 			case 1:
+				System.out.println();
 				buscarSerieWeb();
 				break;
 			case 2:
+				System.out.println();
 				buscarEpisodioPorSerie();
 				break;
 			case 3:
+				System.out.println();
 				listarSeriesBuscadas();
 				break;
 			case 0:
@@ -66,6 +71,7 @@ public class Principal {
 	private DadosSerie getDadosSerie() {
 		System.out.println("Digite o nome da série para busca");
 		var nomeSerie = leitura.nextLine();
+		System.out.println();
 		var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		return dados;
@@ -84,7 +90,15 @@ public class Principal {
 	}
 
 	private void listarSeriesBuscadas() {
-		seriesBuscadas.forEach(System.out::println);
+		List<Serie> series = new ArrayList<Serie>();
+		series = seriesBuscadas.stream()
+				.map(ds -> new Serie(ds))
+				.collect(Collectors.toList());
+		
+		series.stream()
+		.sorted(Comparator.comparing(Serie::getGenero))
+		.forEach(System.out::println);
+		
 		
 	}
 }
